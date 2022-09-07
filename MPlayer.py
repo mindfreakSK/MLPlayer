96a4f6f65d6a451365824d394d1913f8c57d638d
mindfreakSK <shazzshaikh3@gmail.com>
2022-09-07 20:24:01 +0530

    Slider bar implementaion

--- MPlayer.py
+++ MPlayer.py
@@ -1,19 +1,68 @@
+#Import libraries
 from ast import Lambda
+from cgitb import text
 from tkinter import *
 from tkinter import filedialog
-import tkinter
+import tkinter.ttk as ttk 
 import pygame
+import time
+from mutagen.mp3 import MP3
 
-
-#creating the root window
+#Creating the root window
 root = Tk()
 root.title('SK Player')
 root.iconbitmap("images\logo.ico")
-root.geometry("500x300")
+root.geometry("500x400")
+
+#Initialize Pygame Mixer
+pygame.mixer.init()
+
+#Song length information
+def play_time():
+       
+       if stopped:
+            return
+       
+       current_time=pygame.mixer.music.get_pos()/1000
+
+       #through temp label
+       converted_current_time=time.strftime('%M:%S',time.gmtime(current_time))       
+
+       #Current playing song
+       #current_song = song_box.curselection()
+       song = song_box.get(ACTIVE)
+       song = f'C:/Users/Shahbaz/Downloads/Songs/{song}'
+       song_mut=MP3(song)
+
+       global song_length
+       song_length=song_mut.info.length
+       converted_song_length = time.strftime('%M:%S', time.gmtime(song_length))
+
+       current_time +=1
+       if int(my_slider.get()) == int(song_length):
+           status_bar.config(text=f'Time Elapsed: {converted_song_length} of {converted_song_length} ')
+
+       elif paused:
+            pass
+
+       elif int(my_slider.get()) == int(current_time):
+           #Update slider to position
+            slider_position = int(song_length)
+            my_slider.config(to=slider_position, value=int(current_time))
+
+            
+       else:
+            slider_position = int(song_length)
+            my_slider.config(to=slider_position, value=int(my_slider.get()))
+            converted_current_time = time.strftime('%M:%S', time.gmtime(int(my_slider.get())))
+            status_bar.config(text=f'Time Elapsed: {converted_current_time} of {converted_song_length} ')
+            next_time=int(my_slider.get())+1
+            my_slider.config(value=next_time)
+            
+       #update time
+       status_bar.after(1000,play_time)
 
 #Add song function
-
-
 def add_song():
     song = filedialog.askopenfilename(
         initialdir='C:/Users/Shahbaz/Downloads/Songs', title='Choose song', filetypes=(('mp3 Files', '*.mp3'),))
@@ -21,8 +70,6 @@ def add_song():
     song_box.insert(END, song)
 
 #Add many song
-
-
 def add_many_song():
     songs = filedialog.askopenfilenames(
         initialdir='C:/Users/Shahbaz/Downloads/Songs', title='Choose song', filetypes=(('mp3 Files', '*.mp3'),))
@@ -30,22 +77,39 @@ def add_many_song():
         song = song.replace("C:/Users/Shahbaz/Downloads/Songs/", "")
         song_box.insert(END, song)
 
+
 #Play Song function
-
-
 def play():
+    global stopped
+    stopped=False
     song = song_box.get(ACTIVE)
     song = f'C:/Users/Shahbaz/Downloads/Songs/{song}'
 
     pygame.mixer.music.load(song)
     pygame.mixer.music.play(loops=0)
 
+    #Calling Play Time Function
+    play_time()   
+
+    #Update Slider to position
+    """slider_position=int(song_length)
+    my_slider.config(to=slider_position,value=0)"""
+
+global stopped
+stopped=False
 
 #Stop playing current song
 def stop():
+    #Reset slider bar
+    status_bar.config(text='')
+    my_slider.config(value=0)
+    #Stop song
     pygame.mixer.music.stop()
     song_box.selection_clear(ACTIVE)
+    status_bar.config(text='')   
 
+    global stopped
+    stopped=True
 
 #Create global pause variable
 global paused
@@ -53,11 +117,14 @@ paused = False
 
 #Delete song
 def delete_songs():
+       stop() 
        song_box.delete(ANCHOR)
        pygame.mixer.music.stop()
 
 
+#Delete all songs
 def delete_all_songs():
+       stop() 
        song_box.config(state=NORMAL)
        song_box.delete(0, END)
        pygame.mixer.music.stop()
@@ -76,9 +143,25 @@ def pause(is_paused):
         pygame.mixer.music.pause()
         paused = True
 
+#Slider function
+def slide(x):
+
+    #slider_label.config(text=f'{int(my_slider.get())} of {int(song_length)}')
+    song = song_box.get(ACTIVE)
+    song = f'C:/Users/Shahbaz/Downloads/Songs/{song}'
+
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0,start=int(my_slider.get()))
+
+
 
 #Next Song play function
 def next_song():
+    
+    #Reset Status and slider Bar
+    status_bar.config(text='')
+    my_slider.config(value=0)
+
     next_one = song_box.curselection()
     next_one = next_one[0]+1
     song = song_box.get(next_one)
@@ -89,10 +172,14 @@ def next_song():
     song_box.activate(next_one)
     song_box.selection_set(next_one, last=None)
 
+
 #Previous Song play function
-
-
 def prev_song():
+
+    #Reset Status and slider Bar
+    status_bar.config(text='')
+    my_slider.config(value=0)
+
     next_one = song_box.curselection()
     next_one = next_one[0]-1
     song = song_box.get(next_one)
@@ -104,8 +191,7 @@ def prev_song():
     song_box.selection_set(next_one, last=None)
 
 
-#Initialize Pygame Mixer
-pygame.mixer.init()
+
 
 song_box = Listbox(root, bg='black', fg='green', width=60,
                    selectbackground='gray', selectforeground='black')
@@ -113,7 +199,6 @@ song_box.pack(pady=20)
 
 
 #Define Player Control Buttons Images
-
 back_btn_img = PhotoImage(file="images/back_btn.png")
 forward_btn_img = PhotoImage(file="images/forward_btn.png")
 play_btn_img = PhotoImage(file="images/play_btn.png")
@@ -159,5 +244,13 @@ remove_song_menu=Menu(my_menu)
 my_menu.add_cascade(label='Remove Songs',menu=remove_song_menu)
 remove_song_menu.add_command(label='Delete a Song From Playlist',command=delete_songs)
 remove_song_menu.add_command(label='Delete a All Song From Playlist', command=delete_all_songs)
+
+#Create Status Bar
+status_bar=Label(root,text='', bd=1, relief=GROOVE, anchor=E)
+status_bar.pack(fill=X,side=BOTTOM,ipady=2)
+
+#Time/Music slider
+my_slider=ttk.Scale(root,from_=0,to_=100, orient=HORIZONTAL,value=0,command=slide,length=360)
+my_slider.pack(pady=30)
 
 root.mainloop()
